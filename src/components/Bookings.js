cf977f8610dd130ca501b2cc453974d32b0ddd98
Amine ELMAROUFI <elmaroufi.amine@gmail.com>
2020-05-05 18:05:19 +0200

    add booking images and details

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Currency from "react-currency-formatter";
 import Map from "pigeon-maps";
 import Marker from "pigeon-marker";
 import Overlay from "pigeon-overlay";
@@ -23,6 +24,7 @@ class Bookings extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectedBooking: null,
       query: {
         price_min: 0,
         price_max: 3000,
@@ -56,69 +58,75 @@ class Bookings extends Component {
 
   search = () => {
     const { query } = this.state;
-    this.props.onGetBookings(query);
+    this.setState({ selectedBooking: null }, () => {
+      this.props.onGetBookings(query);
+    });
   };
 
   render() {
-    const { query, disabled } = this.state;
+    const { query, disabled, selectedBooking } = this.state;
     const { bookings } = this.props;
+    console.log("selectedBooking:", selectedBooking);
     return (
       <div>
-        {/*  filter */}
-        <form style={{ paddingLeft: "20px" }}>
-          <div className="" style={{ padding: "15px" }}>
-            <div className="form-row align-items-center">
-              <div className="col-2 ">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Department"
-                  value={query.dep}
-                  onChange={(e) => this._handleChange("dep", e.target.value)}
-                />
-              </div>
-              <div className="col-2 ">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Price min"
-                  value={query.price_min}
-                  onChange={(e) =>
-                    this._handleChange("price_min", e.target.value)
-                  }
-                />
-              </div>
-              <div className="col-2 ">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Price max"
-                  value={query.price_max}
-                  onChange={(e) =>
-                    this._handleChange("price_max", e.target.value)
-                  }
-                />
-              </div>
-              <div className="col-3" style={{ marginTop: "5px" }}>
-                <div className="form-check">
+        <div>
+          {/*  filter */}
+          <form className="form_container" style={{ paddingLeft: "20px" }}>
+            <div className="" style={{ padding: "15px" }}>
+              <h5 style={{ marginLeft: "25px" }}>Filter bookings by :</h5>
+              <div className="align-items-center">
+                <div className="input-container">
                   <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="autoSizingCheck2"
-                    checked={query.furnished}
+                    type="text"
+                    className="form-control"
+                    placeholder="Department"
+                    value={query.dep}
+                    onChange={(e) => this._handleChange("dep", e.target.value)}
+                  />
+                </div>
+                <div className="input-container">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Price min"
+                    value={query.price_min}
                     onChange={(e) =>
-                      this._handleChange("furnished", !query.furnished)
+                      this._handleChange("price_min", e.target.value)
                     }
                   />
-                  <label
-                    className="form-check-label"
-                    htmlFor="autoSizingCheck2"
-                    style={{ fontSize: "13px" }}
-                  >
-                    Furnished
-                  </label>
                 </div>
-                <div className="">
+                <div className="input-container">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Price max"
+                    value={query.price_max}
+                    onChange={(e) =>
+                      this._handleChange("price_max", e.target.value)
+                    }
+                  />
+                </div>
+                <div className="input-container" style={{ marginTop: "5px" }}>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      id="autoSizingCheck2"
+                      checked={query.furnished}
+                      onChange={(e) =>
+                        this._handleChange("furnished", !query.furnished)
+                      }
+                    />
+                    <label
+                      className="form-check-label"
+                      htmlFor="autoSizingCheck2"
+                      style={{ fontSize: "13px" }}
+                    >
+                      Furnished
+                    </label>
+                  </div>
+                </div>
+                <div className="btn_container">
                   <button
                     type="button"
                     className="btn btn-primary search-btn"
@@ -136,25 +144,62 @@ class Bookings extends Component {
                 </div>
               </div>
             </div>
+          </form>
+          <div className="map_container">
+            <Map
+              center={[48.864716, 2.349014]}
+              zoom={12}
+              width={600}
+              height={400}
+            >
+              {bookings.length &&
+                bookings.map((book) => {
+                  return (
+                    <Marker
+                      key={book.id}
+                      anchor={[book.location.lat, book.location.lng]}
+                      payload={1}
+                      onClick={({ event, anchor, payload }) =>
+                        this.setState({ selectedBooking: book })
+                      }
+                    />
+                  );
+                })}
+
+              <Overlay anchor={[48.864716, 2.349014]} offset={[120, 79]}>
+                <img src="pigeon.jpg" width={240} height={158} alt="" />
+              </Overlay>
+            </Map>
           </div>
-        </form>
-        <Map center={[48.864716, 2.349014]} zoom={12} width={600} height={400}>
-          {bookings.length &&
-            bookings.map((book) => {
+        </div>
+        <div className="img_container">
+          {selectedBooking &&
+            selectedBooking.images.length &&
+            selectedBooking.images.map((img) => {
               return (
-                <Marker
-                  key={book.id}
-                  anchor={[book.location.lat, book.location.lng]}
-                  payload={1}
-                  onClick={({ event, anchor, payload }) => {}}
+                <img
+                  alt={"test"}
+                  style={{ height: "200px", width: "300px" }}
+                  src={img}
                 />
               );
             })}
 
-          <Overlay anchor={[48.864716, 2.349014]} offset={[120, 79]}>
-            <img src="pigeon.jpg" width={240} height={158} alt="" />
-          </Overlay>
-        </Map>
+          <div style={{ marginTop: "20px" }}>
+            {selectedBooking && selectedBooking.location && (
+              <span style={{ marginRight: "10px" }}>
+                City: {selectedBooking.location.city}
+              </span>
+            )}
+            <br />
+            {selectedBooking && selectedBooking && (
+              <span>
+                Price:{" "}
+                <Currency quantity={selectedBooking.price} currency="EUR" />{" "}
+              </span>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
